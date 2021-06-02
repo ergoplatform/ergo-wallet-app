c9ebe3273d62f8424cb5830aee709af5f8cf1343
Benjamin Schulte <bs@golfgl.de>
2021-06-02 18:16:06 +0200

    Support Action Bar setup

--- app/src/main/java/org/ergoplatform/android/MainActivity.kt
+++ app/src/main/java/org/ergoplatform/android/MainActivity.kt
@@ -14,15 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_transactions, R.id.navigation_wallet, R.id.navigation_settings))
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-        //supportActionBar?.hide()
+                R.id.navigation_wallet, R.id.navigation_settings))
+        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
+    }
 }
