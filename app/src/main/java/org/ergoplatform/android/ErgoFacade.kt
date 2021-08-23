dc03cb9e9fdd6c8c1d85440b88fd473717e1e503
Benjamin Schulte <bs@golfgl.de>
2021-08-23 15:40:06 +0200

    POC EIP19 cold wallet

--- app/src/main/java/org/ergoplatform/android/ErgoFacade.kt
+++ app/src/main/java/org/ergoplatform/android/ErgoFacade.kt
@@ -8,6 +8,7 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import org.ergoplatform.appkit.*
 import org.ergoplatform.appkit.impl.ErgoTreeContract
+import org.ergoplatform.restapi.client.Parameters
 import org.ergoplatform.wallet.mnemonic.WordList
 import scala.collection.JavaConversions
 import java.math.RoundingMode
@@ -19,6 +20,8 @@ import kotlin.math.pow
 
 val MNEMONIC_WORDS_COUNT = 15
 val MNEMONIC_MIN_WORDS_COUNT = 12
+val ERG_BASE_COST = 0
+val ERG_MAX_BLOCK_COST = 1000000
 
 fun nanoErgsToErgs(nanoErgs: Long): Float {
     val microErgs = nanoErgs / (1000L * 100L)
@@ -176,8 +179,112 @@ fun sendErgoTx(
     }
 }
 
+/**
+ * Prepares and serializes a transaction to be transferred to a cold wallet (EIP19)
+ */
+fun prepareSerializedErgoTx(
+    recipient: Address,
+    amountToSend: Long,
+    tokensToSend: List<ErgoToken>,
+    senderAddresses: List<Address>,
+    nodeApiAddress: String,
+    explorerApiAddress: String
+): SerializationResult {
+    try {
+        val ergoClient = RestApiErgoClient.create(
+            nodeApiAddress,
+            StageConstants.NETWORK_TYPE,
+            "",
+            explorerApiAddress
+        )
+        val reduced = ergoClient.execute { ctx: BlockchainContext ->
+            val contract: ErgoContract = ErgoTreeContract(recipient.ergoAddress.script())
+            val unsigned = BoxOperations.putToContractTxUnsigned(
+                ctx, senderAddresses,
+                contract, amountToSend, tokensToSend
+            )
+
+            val reduced = ctx.newProverBuilder().build().reduce(unsigned, ERG_BASE_COST)
+
+            return@execute reduced
+        }
+
+        return SerializationResult(true, reduced.toBytes())
+    } catch (t: Throwable) {
+        Log.e("Send", "Error creating transaction", t)
+        return SerializationResult(false, errorMsg = t.message)
+    }
+}
+
+/**
+ * Deserializes an unsigned transaction, signs it and serializes the signed transaction
+ */
+fun signSerializedErgoTx(
+    serializedTx: ByteArray,
+    mnemonic: String,
+    mnemonicPass: String,
+    derivedKeyIndex: Int
+): SerializationResult {
+    try {
+        val coldClient = ColdErgoClient(
+            StageConstants.NETWORK_TYPE,
+            Parameters().maxBlockCost(ERG_MAX_BLOCK_COST)
+        )
+
+        val signedTxSerialized = coldClient.execute { ctx ->
+            val prover = ctx.newProverBuilder()
+                .withMnemonic(
+                    SecretString.create(mnemonic),
+                    SecretString.create(mnemonicPass)
+                )
+                .withEip3Secret(derivedKeyIndex)
+                .build()
+            val reducedTx = ctx.parseReducedTransaction(serializedTx)
+            return@execute prover.signReduced(reducedTx, ERG_BASE_COST).toBytes()
+        }
+        return SerializationResult(true, signedTxSerialized)
+    } catch (t: Throwable) {
+        Log.e("Send", "Error creating transaction", t)
+        return SerializationResult(false, errorMsg = t.message)
+    }
+}
+
+/**
+ * Sends a serialized and signed transaction
+ */
+fun sendSignedErgoTx(
+    signedTxSerialized: ByteArray,
+    nodeApiAddress: String,
+    explorerApiAddress: String
+): TransactionResult {
+    try {
+        val ergoClient = RestApiErgoClient.create(
+            nodeApiAddress,
+            StageConstants.NETWORK_TYPE,
+            "",
+            explorerApiAddress
+        )
+        val txId = ergoClient.execute { ctx ->
+            val signedTx = ctx.parseSignedTransaction(signedTxSerialized)
+            ctx.sendTransaction(signedTx)
+        }
+
+        return TransactionResult(txId.isNotEmpty(), txId)
+
+    } catch (t: Throwable) {
+        Log.e("Send", "Error creating transaction", t)
+        return TransactionResult(false, errorMsg = t.message)
+    }
+}
+
 data class TransactionResult(
     val success: Boolean,
     val txId: String? = null,
+    val errorMsg: String? = null
+)
+
+data class SerializationResult(
+    val success: Boolean,
+    val serializedTx: ByteArray? = null,
     val errorMsg: String? = null
 )
