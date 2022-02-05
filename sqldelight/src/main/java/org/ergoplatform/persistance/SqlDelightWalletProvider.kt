ergoplatform/ergo-wallet-app

commit d3c94a0a7e5acf623efecc966e7c735e8e025076

Prevent adding the same address twice, closes #86

sqldelight/src/main/java/org/ergoplatform/persistance/SqlDelightWalletProvider.kt
@@ -58,14 +58,21 @@ class SqlDelightWalletProvider(private val appDb: AppDatabase) : WalletDbProvide
         updateWalletConfig(walletConfig)
     }
 
+    override suspend fun deleteWalletConfigAndStates(firstAddress: String, walletId: Int?) {
+        appDb.walletStateQueries.deleteByFirstAddress(firstAddress)
+        appDb.walletTokenQueries.deleteTokensByFirstAddress(firstAddress)
+        appDb.walletAddressQueries.deleteWalletAddressByFirstAddress(firstAddress)
+
+        (walletId ?: loadWalletByFirstAddress(firstAddress)?.id)?.let { id ->
+            appDb.walletConfigQueries.deleteWalletById(id.toLong())
+        }
+    }
+
     suspend fun deleteAllWalletData(walletConfig: WalletConfig) {
         withTransaction {
             walletConfig.firstAddress?.let { firstAddress ->
-                appDb.walletStateQueries.deleteByFirstAddress(firstAddress)
-                appDb.walletTokenQueries.deleteTokensByFirstAddress(firstAddress)
-                appDb.walletAddressQueries.deleteWalletAddressByFirstAddress(firstAddress)
-            }
-            appDb.walletConfigQueries.deleteWalletById(walletConfig.id.toLong())
+                deleteWalletConfigAndStates(firstAddress, walletConfig.id)
+            } ?: appDb.walletConfigQueries.deleteWalletById(walletConfig.id.toLong())
         }
     }
 
@@ -142,6 +149,12 @@ class SqlDelightWalletProvider(private val appDb: AppDatabase) : WalletDbProvide
     override suspend fun loadWalletAddress(id: Long): WalletAddress? {
         return withContext(Dispatchers.IO) {
             appDb.walletAddressQueries.loadWalletAddress(id).executeAsOneOrNull()?.toModel()
+        }
+    }
+
+    override suspend fun loadWalletAddress(publicAddress: String): WalletAddress? {
+        return withContext(Dispatchers.IO) {
+            appDb.walletAddressQueries.loadWalletAddressByPk(publicAddress).executeAsOneOrNull()?.toModel()
         }
     }
 

android/src/main/java/org/ergoplatform/android/AppDatabase.kt
@@ -91,6 +91,15 @@ class RoomWalletDbProvider(val database: AppDatabase) : WalletDbProvider {
         database.walletDao().insertAll(walletConfig.toDbEntity())
     }
 
+    override suspend fun deleteWalletConfigAndStates(firstAddress: String, walletId: Int?) {
+        database.walletDao().deleteWalletStates(firstAddress)
+        database.walletDao().deleteTokensByWallet(firstAddress)
+        database.walletDao().deleteWalletAddresses(firstAddress)
+        (walletId ?: database.walletDao().loadWalletByFirstAddress(firstAddress)?.id)?.let { id ->
+            database.walletDao().deleteWalletConfig(id)
+        }
+    }
+
     override fun getAllWalletConfigsSynchronous(): List<WalletConfig> {
         return database.walletDao().getAllWalletConfigsSyncronous().map { it.toModel() }
     }
@@ -118,6 +127,10 @@ class RoomWalletDbProvider(val database: AppDatabase) : WalletDbProvider {
 
     override suspend fun loadWalletAddress(id: Long): WalletAddress? {
         return database.walletDao().loadWalletAddress(id.toInt())?.toModel()
+    }
+
+    override suspend fun loadWalletAddress(publicAddress: String): WalletAddress? {
+        return database.walletDao().loadWalletAddress(publicAddress)?.toModel()
     }
 
     override suspend fun insertWalletAddress(walletAddress: WalletAddress) {

android/src/main/java/org/ergoplatform/android/wallet/WalletConfigViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.ergoplatform.android.AppDatabase
 import org.ergoplatform.android.R
+import org.ergoplatform.android.RoomWalletDbProvider
 import org.ergoplatform.deserializeSecrets
 import org.ergoplatform.android.ui.SingleLiveEvent
 import org.ergoplatform.api.AesEncryptionManager
@@ -52,11 +53,8 @@ class WalletConfigViewModel : ViewModel() {
             walletConfig?.let {
                 database.withTransaction {
                     walletConfig.firstAddress?.let { firstAddress ->
-                        walletDao.deleteWalletStates(firstAddress)
-                        walletDao.deleteTokensByWallet(firstAddress)
-                        walletDao.deleteWalletAddresses(firstAddress)
+                        RoomWalletDbProvider(database).deleteWalletConfigAndStates(firstAddress, walletId)
                     }
-                    walletDao.deleteWalletConfig(walletId)
                 }
 
                 // After we deleted a wallet, we can prune the keystore if it is not needed

android/src/main/java/org/ergoplatform/android/wallet/WalletDbDao.kt
@@ -56,6 +56,9 @@ interface WalletDbDao {
     @Query("SELECT * FROM wallet_addresses WHERE id = :id")
     suspend fun loadWalletAddress(id: Int): WalletAddressDbEntity?
 
+    @Query("SELECT * FROM wallet_addresses WHERE public_address = :publicAddress")
+    suspend fun loadWalletAddress(publicAddress: String): WalletAddressDbEntity?
+
     @Query("UPDATE wallet_addresses SET label = :label WHERE id = :id")
     suspend fun updateWalletAddressLabel(id: Int, label: String?)
 

android/src/main/res/values/strings.xml
@@ -48,6 +48,7 @@
     <string name="label_erg_price">1 ERG ≈</string>
     <string name="label_fiat_amount">≈ %1s</string>
     <string name="label_wallet_default">My wallet</string>
+    <string name="label_readonly_wallet_default">My wallet (Read only)</string>
     <string name="hint_signing_request">Please scan the signing request from the appropriate
        Wallet\'s Send funds screen.</string>
     <string name="error_qr_code_content_unknown">The scanned QR code was not of a known format.</string>
@@ -66,6 +67,7 @@
     <!-- add readonly wallet -->
     <string name="intro_add_readonly">Enter your public wallet address to add the wallet in read-only mode.</string>
     <string name="label_wallet_address">Wallet address</string>
+    <string name="error_address_already_added">Address already added to \'%1s\'</string>
 
     <!-- Create wallet -->
     <string name="intro_create_wallet">This is your 15-word Ergo wallet mnemonic. These words are also known as a seed phrase or

common-jvm/src/main/java/org/ergoplatform/persistance/WalletDbProvider.kt
@@ -12,6 +12,7 @@ interface WalletDbProvider {
     suspend fun loadWalletConfigById(id: Int): WalletConfig?
     suspend fun updateWalletConfig(walletConfig: WalletConfig)
     suspend fun insertWalletConfig(walletConfig: WalletConfig)
+    suspend fun deleteWalletConfigAndStates(firstAddress: String, walletId: Int? = null)
     fun getAllWalletConfigsSynchronous(): List<WalletConfig>
 
     // State functions
@@ -23,6 +24,7 @@ interface WalletDbProvider {
     // Address functions
     suspend fun loadWalletAddresses(firstAddress: String): List<WalletAddress>
     suspend fun loadWalletAddress(id: Long): WalletAddress?
+    suspend fun loadWalletAddress(publicAddress: String): WalletAddress?
     suspend fun insertWalletAddress(walletAddress: WalletAddress)
     suspend fun updateWalletAddressLabel(addrId: Long, newLabel: String?)
     suspend fun deleteWalletAddress(addrId: Long)

common-jvm/src/main/java/org/ergoplatform/uilogic/StringResources.kt
@@ -96,6 +96,7 @@ const val STRING_DEVICE_ENC_SECURITY_NONE = "device_enc_security_none"
 const val STRING_DEVICE_ENC_SECURITY_PASS = "device_enc_security_pass"
 const val STRING_ERR_PASSWORD = "err_password"
 const val STRING_ERR_PASSWORD_CONFIRM = "err_password_confirm"
+const val STRING_ERROR_ADDRESS_ALREADY_ADDED = "error_address_already_added"
 const val STRING_ERROR_AMOUNT = "error_amount"
 const val STRING_ERROR_BALANCE_ERG = "error_balance_erg"
 const val STRING_ERROR_CAMERA = "error_camera"
@@ -175,6 +176,7 @@ const val STRING_LABEL_PURPOSE = "label_purpose"
 const val STRING_LABEL_QR_CODE = "label_qr_code"
 const val STRING_LABEL_QR_PAGES_INFO = "label_qr_pages_info"
 const val STRING_LABEL_READONLY_WALLET = "label_readonly_wallet"
+const val STRING_LABEL_READONLY_WALLET_DEFAULT = "label_readonly_wallet_default"
 const val STRING_LABEL_RECEIVER_ADDRESS = "label_receiver_address"
 const val STRING_LABEL_REMOVE = "label_remove"
 const val STRING_LABEL_RESTORE_WALLET = "label_restore_wallet"

common-jvm/src/main/java/org/ergoplatform/uilogic/wallet/AddReadOnlyWalletUiLogic.kt
@@ -3,12 +3,14 @@ package org.ergoplatform.uilogic.wallet
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.ergoplatform.NodeConnector
 import org.ergoplatform.isValidErgoAddress
 import org.ergoplatform.persistance.WalletConfig
 import org.ergoplatform.persistance.WalletDbProvider
+import org.ergoplatform.uilogic.STRING_ERROR_ADDRESS_ALREADY_ADDED
 import org.ergoplatform.uilogic.STRING_ERROR_RECEIVER_ADDRESS
-import org.ergoplatform.uilogic.STRING_LABEL_WALLET_DEFAULT
+import org.ergoplatform.uilogic.STRING_LABEL_READONLY_WALLET_DEFAULT
 import org.ergoplatform.uilogic.StringProvider
 
 abstract class AddReadOnlyWalletUiLogic(val stringProvider: StringProvider) {
@@ -17,10 +19,26 @@ abstract class AddReadOnlyWalletUiLogic(val stringProvider: StringProvider) {
             setErrorMessage(stringProvider.getString(STRING_ERROR_RECEIVER_ADDRESS))
             return false
         } else {
+            if (!runBlocking {
+                    val derivedAddress = walletDbProvider.loadWalletAddress(walletAddress)
+                    if (derivedAddress != null) {
+                        setErrorMessage(
+                            stringProvider.getString(
+                                STRING_ERROR_ADDRESS_ALREADY_ADDED,
+                                walletDbProvider.loadWalletByFirstAddress(derivedAddress.walletFirstAddress)?.displayName
+                                    ?: ""
+                            )
+                        )
+                        return@runBlocking false
+                    } else return@runBlocking true
+                }) {
+                return false
+            }
+
             val walletConfig =
                 WalletConfig(
                     0,
-                    stringProvider.getString(STRING_LABEL_WALLET_DEFAULT),
+                    stringProvider.getString(STRING_LABEL_READONLY_WALLET_DEFAULT),
                     walletAddress,
                     0,
                     null

commit bb449298507e154a003f54292b581f3ac53f4a95

Fix freeze on iOS when adding derived addresses, caused by d3c94a0a7

sqldelight/src/main/java/org/ergoplatform/persistance/SqlDelightWalletProvider.kt
@@ -159,7 +159,8 @@ class SqlDelightWalletProvider(private val appDb: AppDatabase) : WalletDbProvide
     }
 
     override suspend fun insertWalletAddress(walletAddress: WalletAddress) {
-        withContext(Dispatchers.IO) {
+        // do not use withContext(Dispatchers.IO) here. Caused freeze on iOS and the only caller
+        // WalletAddressesUiLogic calls in IO context anyway.
             appDb.walletAddressQueries.insertOrReplace(
                 if (walletAddress.id > 0) walletAddress.id else null,
                 walletAddress.walletFirstAddress,
@@ -167,7 +168,6 @@ class SqlDelightWalletProvider(private val appDb: AppDatabase) : WalletDbProvide
                 walletAddress.publicAddress,
                 walletAddress.label
             )
-        }
     }
 
     override suspend fun updateWalletAddressLabel(addrId: Long, newLabel: String?) {