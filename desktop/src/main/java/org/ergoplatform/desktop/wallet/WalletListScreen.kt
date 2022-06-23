1adc46f51c891dc0c2c9136851e1b22137a011ba
Benjamin Schulte <bs@golfgl.de>
2022-06-23 21:46:15 +0200

    Desktop: Add scrollbar to wallet list

--- desktop/src/main/java/org/ergoplatform/desktop/wallet/WalletListScreen.kt
+++ desktop/src/main/java/org/ergoplatform/desktop/wallet/WalletListScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.ergoplatform.desktop.ui.scrollbar
 import org.ergoplatform.persistance.Wallet
 import org.ergoplatform.persistance.WalletConfig
 
@@ -24,8 +27,16 @@ fun WalletListScreen(
         LinearProgressIndicator(Modifier.fillMaxWidth())
     }
 
+    val state = rememberLazyListState()
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
+        state = state,
+        modifier = Modifier.fillMaxSize().scrollbar(
+            state,
+            false,
+            hiddenAlpha = .8f,
+            knobColor = Color.White,
+            trackColor = Color.Transparent
+        ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
