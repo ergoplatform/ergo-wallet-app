bb079c54ac0620dca022d4ab6ab382c7da698f01
Benjamin Schulte <bs@golfgl.de>
2022-10-18 14:24:20 +0200

    Explorer API use concise param ergoplatform/explorer-backend#193

--- common-jvm/src/main/java/org/ergoplatform/ApiServiceManager.kt
+++ common-jvm/src/main/java/org/ergoplatform/ApiServiceManager.kt
@@ -63,8 +63,7 @@ open class ApiServiceManager(
         limit: Int,
         offset: Int
     ): Call<Items<TransactionInfo>> =
-        // TODO concise should be true when https://github.com/ergoplatform/explorer-backend/issues/193 is fixed
-        defaultApi.getApiV1AddressesP1Transactions(publicAddress, offset, limit, false)
+        defaultApi.getApiV1AddressesP1Transactions(publicAddress, offset, limit, true)
 
     override fun checkToken(tokenId: String, tokenName: String): Call<TokenCheckResponse> =
         tokenVerificationApi.checkToken(tokenId, tokenName.replace("/", "-").replace("|", "-"))
