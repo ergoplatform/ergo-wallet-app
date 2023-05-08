57dcbe7a9075431c0bb88255522ec413e3d68e40
Benjamin Schulte <bs@golfgl.de>
2023-05-08 10:20:26 +0200

    Spectrum/ErgoDex Price Info URL changed, closes #168

--- common-jvm/src/main/java/org/ergoplatform/api/ergodex/ErgoDexApi.java
+++ common-jvm/src/main/java/org/ergoplatform/api/ergodex/ErgoDexApi.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ErgoDexApi {
-    @GET("v1/amm/markets")
-    Call<List<ErgoDexSwap>> getSwaps(@Query("from") long from);
+    @GET("v1/price-tracking/markets")
+    Call<List<ErgoDexSwap>> getSwaps();
 
 }
